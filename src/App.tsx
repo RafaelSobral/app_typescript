8c1fb52626b6b45d6d21f4edee5622834ccb7c65
Rafael Sobral <rafaelsobral@outlook.com>
2023-03-02 22:09:50 -0300

    feat: Adicionado React-Router-Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-     TESTE APP
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   );
 }
 
